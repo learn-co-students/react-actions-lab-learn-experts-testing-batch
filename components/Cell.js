4fd2d8a71cbde4606bf44352b6de00fa4767e8a9
kwebster2 <kevin.r.webster@gmail.com>
2017-01-07 17:51:03 +0900

    done

--- components/Cell.js
+++ components/Cell.js
@@ -4,6 +4,12 @@ const React = require('react');
 
 class Cell extends React.Component {
   render () {
+    const { onChange, onFocus } = this.props;
+    return(
+      <td className="cell">
+        <input value={this.props.cell} onChange={onChange} onFocus={onFocus}/>
+      </td>
+    )
   }
 }
 
--- components/Row.js
+++ components/Row.js
@@ -5,6 +5,14 @@ const Cell = require('./Cell');
 
 class Row extends React.Component {
   render () {
+    const { row, onChange, onFocus } = this.props;
+    return(
+      <tr className="row">
+        {row.map((cell, index) => {
+          return <Cell onChange={onChange.bind(null, index)} onFocus={onFocus.bind(null, index)} cell={cell} key={index}/>
+        })}
+      </tr>
+    )
   }
 }
 
