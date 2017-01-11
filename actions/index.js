98ac8c1e4747773da142681614f56bbf1f861a61
Matthew <pickledyamsman@gmail.com>
2017-01-11 14:15:36 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,63 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault()
+  let table = this.state.table
+  table.forEach(arr => arr.push(""))
+  this.setState({
+    table: table
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault()
+  let table = this.state.table
+  let newRow = Array(table[0].length)
+  table.push(newRow.fill(""))
+  this.setState({
+    table: table
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  let table = this.state.table
+  table[rowIndex][columnIndex] = ev.target.value
+  this.setState({
+    table: table
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault()
+  let table = this.state.table
+  if(table.length > 1){
+    this.setState({
+      table: table.slice(0, -1)
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault()
+  let table = this.state.table
+  if(table[0].length > 1){
+    table.forEach(arr => arr.pop())
+    this.setState({
+      table: table
+    })
+  }
 }
 
 module.exports = {
