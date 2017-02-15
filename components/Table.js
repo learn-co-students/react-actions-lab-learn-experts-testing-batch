c384fbd902c79d088fdbd4ef6bdc76de63fe1a88
Traci Ross <tracir@gmail.com>
2017-02-15 20:57:20 +0000

    Done.

--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,21 @@ const Row = require('./Row');
 
 class Table extends React.Component {
   render () {
+    return(
+      <table className="table" onBlur={this.props.onBlur}>
+        <tbody>
+          {this.props.table.map (
+            (row, rowNumber) =>
+              <Row
+                row={row}
+                key={rowNumber}
+                onChange={this.props.onChange.bind(null, rowNumber)}
+                onFocus={this.props.onFocus.bind(null, rowNumber)}
+              />
+          )}
+        </tbody>
+      </table>
+    );
   }
 }
 
