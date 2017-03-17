6c82b22a4805bd3e7cecc97dc92728502780ab73
NStephenson <nickstephenson90@gmail.com>
2017-03-17 12:24:14 -0400

    real eyes realize real lies

--- components/Cell.js
+++ components/Cell.js
@@ -4,6 +4,14 @@ const React = require('react');
 
 class Cell extends React.Component {
   render () {
+    return (
+      <td className="cell">
+        <input type="text" 
+               value={this.props.cell} 
+               onChange={this.props.onChange} 
+               onFocus={this.props.onFocus} />
+      </td>
+    )
   }
 }
 
